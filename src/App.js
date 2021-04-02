dd8d7270bc95207e55a0ef5a17dedbca42ebcc8d
yooperjb <jmb181@humboldt.edu>
2021-04-02 15:23:15 -0700

    initial map setup. token not working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import ReactMapGl, {Marker, Popup} from 'react-map-gl';
 import './App.css';
 
 function App() {
+
+  const [viewport, setViewport] = useState({
+    latitude: 40.7450,
+    longitude: -123.8695,
+    zoom: 11,
+    width: '100vw',
+    height: '100vh'
+  });
+
   return (
-    <div className="App">
-      Map goes here
-    </div>
+    <ReactMapGl
+    {...viewport}
+    mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+    onViewportChange={setViewport}
+    >
+
+    </ReactMapGl>
   );
 }
 
