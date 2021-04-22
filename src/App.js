fc3dfc8ebd2e481bb42428897f015633db6d6cb4
yooperjb <jmb181@humboldt.edu>
2021-04-22 10:40:44 -0700

    adding layers practice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import ReactMapGl, {Marker, Popup} from 'react-map-gl';
+import ReactMapGl, {Marker, Popup, Source, Layer} from 'react-map-gl';
 import './App.css';
+
+const bikePoints = {
+  name: 'bike-points',
+  type: 'vector',
+  url: 'mapbox://yooperjb.96kntbve',
+};
+
+const bikePointsStyle = {
+  id: "bike-points", 
+  type: "circle",
+  source: 'bike-points',
+  "source-layer": "bike_points-8mbmdl", 
+  paint: {
+    "circle-radius": 3,
+    "circle-color": '#5776f2'
+  }
+}
+
+const waterPoints = {
+  name: 'water-points',
+  type: 'vector',
+  url: "mapbox://yooperjb.d3fmw0q7"
+};
+
+const waterStyle = {
+  id: "water-points", 
+  type: "circle",
+  source: 'water-points',
+  "source-layer": "water_points-d2zqcw", 
+  paint: {
+    "circle-radius": 4,
+    "circle-color": '#5776f2'
+  }
+}
+
+console.log(waterPoints);
+console.log({...waterPoints})
 
 function App() {
 
   const [viewport, setViewport] = useState({
     latitude: 40.7450,
     longitude: -123.8695,
-    zoom: 11,
+    zoom: 8,
     width: '100vw',
     height: '100vh'
   });
@@ -18,6 +55,14 @@ function App() {
     mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
     onViewportChange={setViewport}
     >
+      <Source {...waterPoints}>
+        <Layer {...waterStyle} />
+      </Source>
+
+      <Source {...bikePoints}>
+        <Layer {...bikePointsStyle} />
+      </Source>
+      
 
     </ReactMapGl>
   );
