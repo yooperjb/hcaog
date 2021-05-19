5fd9d07050b061321a1560f10c5e19da9c3327dd
Pedal <jmb181@humboldt.edu>
2021-05-18 21:39:12 -0700

    add more checkboxes to sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -27,6 +27,33 @@ const Sidebar = () => {
                 <input type="checkbox" id="trail" name="trail" value="trail"></input>
                 <label for="trail">Trails</label>
                 </div>
+                <div>
+                <input type="checkbox" id="bikeshop" name="bikeshop" value="bikeshop"></input>
+                <label for="bikeshop">Bike Shops</label>
+                </div>
+                <div>
+                <input type="checkbox" id="parking" name="parking" value="parking"></input>
+                <label for="parking">Bike Parking</label>
+                </div>
+                <div>
+                <input type="checkbox" id="toolstation" name="toolstation" value="toolstation"></input>
+                <label for="toolstation">Tool Station</label>
+                </div>
+            </div>
+            <div className="layers">
+                <h3>Connector Routes</h3>
+                <div>
+                <input type="checkbox" id="familyfriendly" name="familyfriendly" value="familyfriendly"></input>
+                <label for="familyfriendly">Family Friendly</label>
+                </div>
+                <div>
+                <input type="checkbox" id="intermidiate" name="intermidiate" value="intermidiate"></input>
+                <label for="intermidiate">Intermidiate</label>
+                </div>
+                <div>
+                <input type="checkbox" id="advanced" name="advanced" value="advanced"></input>
+                <label for="advanced">Advanced</label>
+                </div>
             </div>
 
         </div>
